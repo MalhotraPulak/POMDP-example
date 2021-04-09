aa3d257f746b8a46b3d263bd9d874361120364e7
PulakIIIT <jimrohn28@gmail.com>
2021-04-09 12:29:23 +0530

    Ending part 1?

--- part1.py
+++ part1.py
@@ -1,9 +1,10 @@
 from typing import List
+from copy import deepcopy
 
-RIGHT = "r"
-LEFT = "l"
-GREEN = "g"
-RED = "r"
+RIGHT = "R"
+LEFT = "L"
+GREEN = "G"
+RED = "R"
 num_states = 6
 roll_no = 2019101050
 x = 1 - ((roll_no % 10000) % 30 + 1) / 100
@@ -12,16 +13,16 @@ y = (roll_no % 100) % 4
 
 def t(old_state, a, curr_state):
     if a == RIGHT:
-        if curr_state == max(num_states - 1, old_state + 1):
+        if curr_state == min(num_states - 1, old_state + 1):
             return x
-        elif curr_state == min(old_state - 1, 0):
+        elif curr_state == max(old_state - 1, 0):
             return 1 - x
         else:
             return 0
     elif a == LEFT:
-        if curr_state == max(num_states - 1, old_state + 1):
+        if curr_state == min(num_states - 1, old_state + 1):
             return 1 - x
-        elif curr_state == min(old_state - 1, 0):
+        elif curr_state == max(old_state - 1, 0):
             return x
         else:
             return 0
@@ -60,19 +61,46 @@ class Belief:
     def __init__(self):
         print("2019101050 2019101049")
         print(x, y)
+        debug = True
         b = [1 / 3, 0, 1 / 3, 0, 0, 1 / 3]
+        printer(b)
         b_prime: List[float] = [0.0 for _ in range(num_states)]
         for a, o in [(RIGHT, GREEN), (LEFT, RED), (LEFT, GREEN)]:
-            printer(b)
             for new_state in range(num_states):
-                b_prime[new_state] = sum(
-                    [t(old_state, a, new_state) * b[old_state] for old_state in range(num_states)])
-                b_prime[new_state] *= O(new_state, a, o)
-                denominator = sum(b_prime)
-                for idx in range(num_states):
-                    b_prime[idx] = b_prime[idx] / denominator
-            b = b_prime
-        printer(b)
+                if debug:
+                    print(f"Calculating b_prime[{new_state}]")
+                # prob_states = [t(old_state, a, new_state) * b[old_state] for old_state in range(num_states)]
+                transitions = [0 for _ in range(num_states)]
+                for old_state in range(num_states):
+                    transitions[old_state] = t(old_state, a, new_state)
+                    if debug:
+                        print(
+                            f"T({old_state}->{new_state},{a}) = {round(transitions[old_state], 3)}")
+                    if transitions[old_state] == 0:
+                        continue
+                    transitions[old_state] *= b[old_state]
+                    if debug:
+                        print(
+                            f"On multiplying by b[{old_state}]={round(b[old_state], 3)} gives {round(transitions[old_state], 3)}")
+                sum_transition = sum(transitions)
+                observation_prob = O(new_state, a, o)
+                b_prime[new_state] = sum_transition * observation_prob
+                if debug:
+                    print("sum of transitions", sum_transition)
+                    print(f"P(o ={o}|a={a},s'={new_state}) = {observation_prob}")
+                    print(f"b_prime[{new_state}]={b_prime[new_state]}")
+                    print()
+            numerator = deepcopy(b_prime)
+            denominator = sum(numerator)
+            for idx in range(num_states):
+                b_prime[idx] = numerator[idx] / denominator
+            if debug:
+                print()
+                print("Not normalized numerator", numerator)
+                print("Denominator", denominator)
+                print("-----------------------------------------")
+            b = deepcopy(b_prime)
+            printer(b)
 
 
 Belief()
